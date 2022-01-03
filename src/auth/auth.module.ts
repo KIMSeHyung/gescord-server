cbc7979895de9fa6c0f3d9ff42b2231492a3033b
KIM Se Hyung <tpgud33@gmail.com>
2022-01-03 21:38:30 +0900

    토큰 검증 유저 캐시 구현

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { JwtStrategy } from './jwt.strategy';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get(JWT_SECRET),
-        signOptions: { expiresIn: '14d' },
+        signOptions: { expiresIn: '7d' },
       }),
       inject: [ConfigService],
     }),
